0795615ccf5b8167c8b1124b08e133d041260f8b
Pirity Máté Róbert <120328031+pruttymate@users.noreply.github.com>
2026-03-27 15:36:53 +0100

    test

--- src/entities/Snowplower.java
+++ src/entities/Snowplower.java
@@ -1,4 +1,27 @@
 package entities;
 
+import user.Cleaner;
+
+import equipment.Head;
+import equipment.HeadInventory;
+
 public class Snowplower extends Vehicle {
+    Cleaner owner;
+    Head activeHead;
+
+    void onTick(){
+        System.out.println("Ontick hívódott");
+
+        System.out.println("Elhagytad a sávot?");
+        boolean elhagyta = false; // TODO
+        if(elhagyta){
+            activeHead.clean();
+        }
+
+        System.out.println("OnTick visszatért");
+
+    }
+    Cleaner getCleaner(){
+
+    }
 }
