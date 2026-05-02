69e4fa87fa5285adb2c957b334862268c672ce0d
Tóth Milán György <76682047+Milan8890@users.noreply.github.com>
2026-05-02 23:10:43 +0200

    összes teszt geci

---
+++ Tesztek/10DragonHeadLeaveCrossing/runtest.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "$0")" &> /dev/null && pwd)
+
+INPUT_FILE="$SCRIPT_DIR/input.txt"
+OUTPUT_FILE="$SCRIPT_DIR/output.txt"
+
+SUCCESS=true
+
+cat "$INPUT_FILE" | java -cp bin main.App > "$OUTPUT_FILE"
+
+if grep -q "[ERROR]" "$OUTPUT_FILE"; then
+	echo "Output still has an [ERROR] in it (severe logging message)."
+	SUCCESS=false
+else
+:
+fi
+
+TESTED_LINE="Dragon1_1 allows Snowplower1_1 entering Lane1_1"
+if grep -q "$TESTED_LINE" "$OUTPUT_FILE"; then
+:
+else
+	SUCCESS=false
+	echo "Didn't find line when it should be present: $TESTED_LINE"
+fi
+
+TESTED_LINE="INFO Snowplower1_1 current lane is Lane1_1"
+if grep -q "$TESTED_LINE" "$OUTPUT_FILE"; then
+:
+else
+	SUCCESS=false
+	echo "Didn't find line when it should be present: $TESTED_LINE"
+fi
+
+if [ "$SUCCESS" = true ]; then
+    echo "Success"
+    exit 0
+else
+    echo "Error"
+    exit 1
+fi
