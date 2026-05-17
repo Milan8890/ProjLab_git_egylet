445dad017d862e4aac4cc1d5957760bc63dffdc1
Falucskai András <86012066+Andros383@users.noreply.github.com>
2026-05-17 17:40:43 +0200

    fix 2

--- src/main/App.java
+++ src/main/App.java
@@ -15,6 +15,7 @@ import java.awt.Rectangle;
 import java.awt.RenderingHints;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
